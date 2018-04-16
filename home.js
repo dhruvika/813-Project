5660db52720ffaefe8506a34f4c076203614fae0
eswar <eswara@mit.edu>
2018-04-16 14:55:27 -0400

    merging feedback with homepage

---
+++ home.js
@@ -0,0 +1,4 @@
+function load_feedback() {
+    console.log("loading feedback")
+     document.getElementById("content1").innerHTML='<object type="text/html" data="parent_feedback.html" ></object>';
+}
