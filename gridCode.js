8c651d881e1a6090e4aa250858fead0238f3f2fc
Apurva Shrivastava <apurvas@mit.edu>
2018-04-18 18:27:09 -0400

    modified grid hovering

--- gridCode.js
+++ gridCode.js
@@ -18,25 +18,8 @@ function grid(x, y) {
 
   var side = square.eq(original - 1).position().left + square.width() + 2; // tighten the #main width
   $("#main").width(side);
-
-  $('.squares').hover(
-    function() {
-      $(this).addClass('hover-general');
-    }
-  )
 }
 
-function gridq() {
-  $('.squares').removeClass('hover-general');
-  $('div').remove('.squares');
-
-  var newgrid = prompt("How many squares on each side?");
-  var widthscreen = 192;
-
-  if (newgrid > 0) {
-    grid(newgrid, newgrid);
-  }
-}
 
 // Attaching events on document because then we can do it without waiting for
 // the DOM to be ready (i.e. before DOMContentLoaded fires)
