281c3041fb09d701c6cf3faacfb46d49b0b47d79
Dhruvika Sahni <dhruvika@mit.edu>
2018-04-18 19:34:49 -0400

    Modified grid code to load student images from class_data.js

--- gridCode.js
+++ gridCode.js
@@ -1,5 +1,9 @@
-var wide = (780 - 15) / 16,
-tall = (780 - 17) / 16; // set the square dimensions. this can be incorporated into the grid() function with 16 replaced by 'original'
+// NOTE: Make sure to not reuse any variable names from class_data.js as it is "global"
+var grid_rows = 10
+var grid_cols = 10
+var wide = (600 - 9) / grid_cols;
+var tall = (600 - 11) / grid_rows; // set the square dimensions. this can be incorporated into the grid() function with 16 replaced by 'original'
+current_class = "class1";
 
 function grid(x, y) {
   var original = x,
@@ -7,9 +11,22 @@ function grid(x, y) {
   $("#main").empty(); // empty and restart
   $("#main").width(wide * (original + 1));
 
+  var current_col = 0;
+  var current_row = 0;
+  var main_div = document.getElementById('main');
   for (var i = 0; i < original * y; i++) {
 
-    $("<div class='squares'></div>").appendTo('#main');
+    var square = document.createElement('div');
+    square.classList.add('squares');
+    main_div.appendChild(square);
+    if(current_col < grid_cols){
+      square.id = "div_r"+current_row+"c"+current_col;
+      current_col += 1;
+    }
+    else{
+      current_row += 1;
+      current_col = 0;
+    }
   }
 
   var square = $(".squares");
@@ -20,6 +37,28 @@ function grid(x, y) {
   $("#main").width(side);
 }
 
+function addStudentImages(){
+  var class_list = class_to_student[current_class];
+  var current_row = 0;
+  var current_col = 0;
+  for (var i = 0; i < class_list.length; i++){
+    var student_name = class_list[i];
+    var student_img_src = student_to_img[student_name];
+    div = document.getElementById("div_r"+current_row+"c"+current_col);
+    student_img = document.createElement('img');
+    if(current_col < grid_cols){      
+      student_img.id = "img_r"+current_row+"c"+current_col;
+      student_img.src = student_img_src;
+      div.appendChild(student_img);
+      current_col += 1;
+    }
+    else {
+      current_row += 1;
+      current_col = 0;     
+    }  
+  }
+}
+
 
 // Attaching events on document because then we can do it without waiting for
 // the DOM to be ready (i.e. before DOMContentLoaded fires)
@@ -28,6 +67,7 @@ Util.events(document, {
   // runs at the end of start-up when the DOM is ready
   "DOMContentLoaded": function() {
     grid(16, 16); // starting dimension
+    addStudentImages();
   }
 });
 
