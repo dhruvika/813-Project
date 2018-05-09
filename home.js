98fe787a11033e0d447d59ea6964f9d4c92fab4c
Dhruvika Sahni <dhruvika@mit.edu>
2018-05-09 00:25:01 -0400

    Fixed bug caused by fixing other bug

--- home.js
+++ home.js
@@ -76,7 +76,7 @@ function load_feedback() {
 		if(previousContent != null && document.getElementById(previousContent) != null) {
 			document.getElementById(previousContent).style["display"] = "none";
 			for(var i=1; i<=classes.length; i++){
-				if(i != classnum) {
+				if(i != classnum && document.getElementById('label'+i) != null) {
 					console.log("Lightening label: ", i)
 					document.getElementById('label'+i).style.filter = "opacity(45%)";
 					document.getElementById('label'+i).style.color = "black";
@@ -141,6 +141,7 @@ function load_feedback() {
 		var page_tabs_html = document.getElementById("page_tabs").innerHTML;
 		window.sessionStorage.setItem("tabs_code", page_tabs_html);
 
+		console.log("Adding new section to: ", class_num)
 		add_new_section(class_num, 'body_home');
 		classClick(class_num);
 
@@ -156,6 +157,7 @@ function load_feedback() {
 		page_element.appendChild(section_tag);
 
 		if(page_type == 'body_home'){
+			console.log("Creating section for body home")
 
 			var all_buttons_tag = document.createElement('div');
 			all_buttons_tag.id = "allButtons";
@@ -197,7 +199,7 @@ function load_feedback() {
 			checkEngagementButtons();
 			checkFeedbackButtons();
 			checkSettingsButtons();
-			// addDeleteClick(class_num);
+			addDeleteClick(class_num);
 
 			var page_sections_html = document.getElementById("page_sections").innerHTML;
 			window.sessionStorage.setItem(page_type+"_code", page_sections_html);
@@ -212,7 +214,7 @@ function load_feedback() {
 			object_element.data = "engage_template.html";
 			section_tag.appendChild(object_element);
 
-			// addDeleteClick(class_num);
+			addDeleteClick(class_num);
 
 			var page_sections_html = document.getElementById("page_sections").innerHTML;
 			window.sessionStorage.setItem(page_type+"_code", page_sections_html);
@@ -226,7 +228,7 @@ function load_feedback() {
 			object_element.data = "parent_feedback.html";
 			section_tag.appendChild(object_element);
 
-			// addDeleteClick(class_num);
+			addDeleteClick(class_num);
 
 			var page_sections_html = document.getElementById("page_sections").innerHTML;
 			window.sessionStorage.setItem(page_type+"_code", page_sections_html);
@@ -240,7 +242,7 @@ function load_feedback() {
 			object_element.data = "gridViewEdit/gridViewEdit.html";
 			section_tag.appendChild(object_element);
 
-			// addDeleteClick(class_num);
+			addDeleteClick(class_num);
 
 			var page_sections_html = document.getElementById("page_sections").innerHTML;
 			window.sessionStorage.setItem(page_type+"_code", page_sections_html);
@@ -266,15 +268,35 @@ function load_feedback() {
 		var class_section = document.getElementById("content"+classnum);
 		class_section.remove();
 
+		// Class Click to previous class
+		if(classes.length == 1){
+			console.log("Only one class left");
+			console.log(classes);
+			classClick(classes[0]);
+		}
+		else if(classes.length > 1){
+			var index = classes.indexOf(classnum);
+			if(index > 0){
+				classClick(classes[index - 1]);
+			}
+			else {
+				console.log("Many classes but deleting first")
+				console.log(classes[index + 1]);
+				classClick(classes[index + 1]);
+			}
+		}
+		
+
 		// Save to session Storage
 		var page_tabs_html = document.getElementById("page_tabs").innerHTML;
 		window.sessionStorage.setItem("tabs_code", page_tabs_html);
 
 		var page_sections_html = document.getElementById("page_sections").innerHTML;
-		window.sessionStorage.setItem("sections_code", page_sections_html);
+
+		var body_tag_id = document.getElementsByTagName("body")[0].id;
+		window.sessionStorage.setItem(body_tag_id+"_code", page_sections_html);
 
 		window.sessionStorage.setItem("classes", JSON.stringify(classes));
-		window.sessionStorage.setItem("total_classes", JSON.stringify(total_classes));
 	}
 
 
@@ -410,10 +432,6 @@ Util.events(document, {
 			var page_sections_html = document.getElementById("page_sections").innerHTML;
 			sessionStorage.setItem("body_home_code", page_sections_html);
 			checkChanges();
-			for(var i in classes){
-				var class_num = parseInt(classes[i]);
-				addDeleteClick(class_num);
-			}
 
 		}
 
